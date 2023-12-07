d19b3d37444f99d3ae76dbc87867acbb0676ab8f
trinm31 <trigcd19833@fpt.edu.vn>
2023-12-07 10:32:53 +0700

    done create form

--- frontend/src/components/create.component.js
+++ frontend/src/components/create.component.js
@@ -1,7 +1,134 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+
+const brands = [
+  {
+    value: "GUCCI",
+    label: "GUCCI",
+  },
+  {
+    value: "LV",
+    label: "LV",
+  },
+  {
+    value: "ADIDAS",
+    label: "ADIDAS",
+  },
+  {
+    value: "NIKE",
+    label: "NIKE",
+  },
+];
+
+const sex = [
+  {
+    value: "Male",
+    label: "Male",
+  },
+  {
+    value: "Female",
+    label: "Female",
+  },
+];
 
 const Create = () => {
-  return <div>Create</div>;
+  const onSubmit = (e) => {};
+
+  return (
+    <>
+      <h1
+        style={{ textAlign: "center", color: "#1976D2", marginBottom: "10px" }}
+      >
+        Create New Shirt
+      </h1>
+      <form
+        style={{
+          diplay: "flex",
+          flexDirection: "column",
+          maxWidth: "800px",
+          margin: "0 auto",
+          marginTop: 3,
+        }}
+        onSubmit={onSubmit}
+      >
+        <TextField
+          label="Name"
+          variant="outlined"
+          name="name"
+          fullWidth
+          required
+          sx={{ marginBottom: 2 }}
+        />
+        <TextField
+          label="Brand"
+          variant="outlined"
+          name="brand"
+          fullWidth
+          required
+          select
+          sx={{ marginBottom: 2 }}
+        >
+          {brands.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        {/* <TextField
+        label="Created Date"
+        variant="outlined"
+        name="createdDate"
+        fullWidth
+        required
+        type="date"
+        sx={{ marginBottom: 2 }}
+      /> */}
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DateTimePicker", "DateTimePicker"]}>
+            <DateTimePicker
+              label="Uncontrolled picker"
+              defaultValue={dayjs("2022-04-17T15:30")}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+
+        <TextField
+          label="Sex"
+          variant="outlined"
+          name="sex"
+          fullWidth
+          required
+          select
+          sx={{ marginBottom: 2, marginTop: 2 }}
+        >
+          {sex.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          label="Price"
+          variant="outlined"
+          name="price"
+          fullWidth
+          required
+          type="number"
+          sx={{ marginBottom: 2 }}
+        />
+        <Button variant="contained" color="primary" type="submit" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default Create;
--- frontend/src/components/index.component.js
+++ frontend/src/components/index.component.js
@@ -6,64 +6,130 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 import { Shirt } from "../apiServices";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 export default function Index() {
   const [data, setData] = useState([]);
+  const navigation = useNavigate();
 
   useEffect(() => {
+    onLoad();
+  }, []);
+
+  const onLoad = () => {
     Shirt.get()
       .then((res) => {
         setData(res.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  };
+
+  const deleteShirt = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Shirt._delete(id)
+          .then((res) => {
+            onLoad();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          })
+          .catch((err) => console.log(err));
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error",
+        });
+      }
+    });
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>No</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Brand</TableCell>
-            <TableCell align="right">Created Date</TableCell>
-            <TableCell align="right">Sex</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data ? (
-            data.map((row, i) => (
-              <TableRow
-                key={i}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {i + 1}
-                </TableCell>
-                <TableCell align="right">{row.name}</TableCell>
-                <TableCell align="right">{row.brand}</TableCell>
-                <TableCell align="right">
-                  {new Date(row.createdDate)
-                    .toISOString()
-                    .slice(0, 19)
-                    .replace(/-/g, "/")
-                    .replace("T", " ")}
-                </TableCell>
-                <TableCell align="right">
-                  {row.sex ? "Male" : "Female"}
-                </TableCell>
-                <TableCell align="right">{row.price}</TableCell>
-                <TableCell align="right">No Action</TableCell>
-              </TableRow>
-            ))
-          ) : (
-            <h1>No Data</h1>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Button
+        onClick={() => navigation("/create")}
+        variant="contained"
+        color="success"
+      >
+        Create
+      </Button>
+      <TableContainer component={Paper} sx={{ marginTop: 2 }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Brand</TableCell>
+              <TableCell align="right">Created Date</TableCell>
+              <TableCell align="right">Sex</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data ? (
+              data.map((row, i) => (
+                <TableRow
+                  key={i}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {i + 1}
+                  </TableCell>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.brand}</TableCell>
+                  <TableCell align="right">
+                    {new Date(row.createdDate)
+                      .toISOString()
+                      .slice(0, 19)
+                      .replace(/-/g, "/")
+                      .replace("T", " ")}
+                  </TableCell>
+                  <TableCell align="right">
+                    {row.sex ? "Male" : "Female"}
+                  </TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      variant="contained"
+                      color="warning"
+                      sx={{ marginRight: 2 }}
+                    >
+                      Update
+                    </Button>
+                    <Button
+                      onClick={() => deleteShirt(row.id)}
+                      variant="contained"
+                      color="error"
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <h1>No Data</h1>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
